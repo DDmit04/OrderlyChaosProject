222dbc123f0e499f0f40ea68d4d40d918266b2c3
DDmit04 <danya.dmit1@gmail.com>
2019-06-30 14:26:11 +0300

    add color tabs component

---
+++ src/main/resources/js/components/pointTabs.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class='form-inline'>
+        <b-tabs class='mt-3 col-sm'>
+            <b-tab title='new point'>
+                <pointSelector :pointArrayType='"new"'/>
+                <pointColors :pointArrayType='"new"'
+                             :restartAllDrawingFunctions='restartAllDrawingFunctions'/>
+            </b-tab>
+            <b-tab title='old point'>
+                <pointSelector :pointArrayType='"old"'/>
+                <pointColors :pointArrayType='"old"'
+                             :restartAllDrawingFunctions='restartAllDrawingFunctions'/>
+            </b-tab>
+            <b-tab title='core point' :disabled='corePoints.length === 0'>
+                <div v-if='corePoints.length > 0'>
+                    <pointSelector :pointArrayType='"core"'/>
+                    <pointColors :pointArrayType="'core'"
+                                 :restartAllDrawingFunctions='restartAllDrawingFunctions'/>
+                </div>
+            </b-tab>
+        </b-tabs>
+    </div>
+</template>
+
+<script>
+    import {mapState} from 'vuex'
+    import pointColors from 'components/pointColors.vue'
+    import pointSelector from 'components/pointSelector.vue'
+
+    export default {
+        name: "pointTabs",
+        props: ['restartAllDrawingFunctions'],
+        components: {
+            pointColors,
+            pointSelector,
+        },
+        computed:
+            mapState(['corePoints'])
+    }
+</script>
+
+<style scoped>
+
+</style>
